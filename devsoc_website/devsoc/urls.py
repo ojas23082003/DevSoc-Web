a0c6a1614916aa5b316b9e45eefd1f92e36528c3
Ojas <ojas.modak23@gmail.com>
2023-01-06 23:40:30 +0530

    Made basic pages for every url

--- devsoc_website/devsoc/urls.py
+++ devsoc_website/devsoc/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('noticeboard', views.noticeboard, name='noticeboard'),
+    path('forum', views.forum, name='forum'),
+    path('login', views.login, name='login'),
+    path('resources', views.resources, name='resources'),
 ]
--- devsoc_website/devsoc/views.py
+++ devsoc_website/devsoc/views.py
@@ -6,3 +6,18 @@ from .models import *
 def home(request):
     a = model.objects.all()
     return render(request, 'index.html', {'a':a})
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def forum(request):
+    return render(request, 'forum.html')
+
+def noticeboard(request):
+    return render(request, 'noticeboard.html')
+
+def resources(request):
+    return render(request, 'resources.html')
